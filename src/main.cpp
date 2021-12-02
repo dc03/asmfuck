d59aff8ea2d4c2e7bca9ceb88fb1958dc7d0f4be
Dhruv Chawla <44582521+dc03@users.noreply.github.com>
2021-12-02 15:50:01 +0530

    Rename interp to asmfuck

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <iterator>
 #include <string>
 
-extern "C" int interp_main(const char *insts);
+extern "C" int asmfuck_main(const char *insts);
 
 int main(int argc, char **argv) {
     std::string file{};
@@ -16,10 +16,10 @@ int main(int argc, char **argv) {
 
     std::ifstream contents{file, std::ios::in};
     if (not contents.is_open()) {
-        std::cout << "bruh\n";
+        std::cout << "File doesn't exist\n";
         return -1;
     }
 
     std::string insts{std::istream_iterator<char>(contents), std::istream_iterator<char>()};
-    interp_main(insts.data());
+    asmfuck_main(insts.data());
 }
